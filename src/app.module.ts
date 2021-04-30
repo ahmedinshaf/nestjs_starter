29a4880b55c8690c5176c595042a15ac147def8e
Inshaf Ahamed <amdinshaf@gmail.com>
2021-04-30 23:47:28 +0530

    student module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 
 @Module({
   imports: [
@@ -11,12 +13,13 @@ import { LessonModule } from './lesson/lesson.module';
       url:
         'mysql://bb981a084f00ed:165f05be@us-cdbr-east-03.cleardb.com/heroku_ac0db10c3a0e007?reconnect=true',
       synchronize: true,
-      entities: [Lesson],
+      entities: [Lesson,Student],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     LessonModule,
+    StudentModule,
   ],
 })
 export class AppModule {}
---
+++ src/student/student.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Student {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+}
